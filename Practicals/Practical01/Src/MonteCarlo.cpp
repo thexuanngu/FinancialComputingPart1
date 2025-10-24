42dbc70a115c5b2f57fa9420afa6a151aa92cf15
Xuanthe <xnguyen@college.harvard.edu>
2025-10-24 17:10:38 +0100

    MonteCarlo2 is giving me segmentation fault. I think my RAM is cooked.

--- Practicals/Practical01/Src/MonteCarlo.cpp
+++ Practicals/Practical01/Src/MonteCarlo.cpp
@@ -24,11 +24,12 @@ namespace exercises {
 
                     // TODO: Implement this!
                     double dSummation(0.0);
-
-                    for(int i; i < iN; i++) {
+                    int j = 0;
+                    for(int i = 0; i < iN; i++) {
                         dSummation += sumTerms(dR, dS0, dSigma, dT, dK);
+                        j++;
                     }
-
+                    std::cout << "Debug: dSummation = " << dSummation << "i = " << j << std::endl;
                     return exp(-dR * dT) * dSummation / iN;
                    }
 
@@ -40,10 +41,31 @@ namespace exercises {
 				   unsigned long int iN, 
 				   Payoff call) {
         MCResult ouput;
-        ouput.mc_estimate = 0.0;
-        ouput.mc_stdev = 0.0;
+
+        ouput.mc_estimate = exp (-dR *dT) * std::accumulate(
+            std::vector<double>(iN).begin(),
+            std::vector<double>(iN).end(),
+            0.0,
+            [&](double current_sum, double element) {
+                double S_T = dS0 * exp(dT * (dR - 0.5 * pow(dSigma, 2)) + dSigma * sqrt(dT) * utils::NormalDist());
+                return current_sum + call(S_T);
+            }
+        ) / iN;
+        std::cout << "Debug: mc_estimate = " << ouput.mc_estimate << std::endl;
+
+        ouput.mc_stdev = exp (-dR *dT) * sqrt((std::accumulate(
+            std::vector<double>(iN).begin(),
+            std::vector<double>(iN).end(),
+            0.0,
+            [&](double current_sum, double element) {
+                double S_T = dS0 * exp(dT * (dR - 0.5 * pow(dSigma, 2)) + dSigma * sqrt(dT) * utils::NormalDist());
+                return current_sum + call(S_T);
+            }
+        ) / iN) - pow(ouput.mc_estimate, 2)) / sqrt(iN);
+        std::cout << "Debug: mc_stdev = " << ouput.mc_stdev << std::endl;
+
         return ouput;
-                   }
+        }
 
     double callAt1(double dS) {
         return 42.0;
